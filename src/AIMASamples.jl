a74fdd3a33f6a1154b26b7632e9a1b83e4b0dc65
Sambit Kumar Dash <sambitdash@gmail.com>
2018-01-07 14:58:35 +0530

    Fixing test arrors.

--- src/AIMASamples.jl
+++ src/AIMASamples.jl
@@ -1,5 +1,13 @@
 module AIMAsamples
 
+try
+    if Pkg.installed("AIMACore") == nothing
+        Pkg.add("AIMACore")
+    end
+catch
+    Pkg.clone("https://github.com/sambitdash/AIMACore.jl.git")
+end
+
 include("VacuumEnvironment.jl")
 include("Romania.jl")
 
